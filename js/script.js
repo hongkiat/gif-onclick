d00a550724f48ca4dc37f82a188818d36b894658
Thoriq Firdaus <tfirdaus@outlook.com>
2015-03-08 20:51:14 +0700

    add function to revert to static file

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 	var getGif = function() {
 		var gif = [];
 		$('img').each(function() {
-			var data = $(this).data('gif');
+			var data = $(this).data('alt');
 			gif.push(data);
 		});
 		return gif;
@@ -18,8 +18,21 @@
 	};
 
 	$('figure').on('click', function() {
-		var $index = $(this).index();
-		$(this).children('img').attr('src', image[$index].src);
+
+		var $this   = $(this),
+				$index  = $this.index(),
+				
+				$img    = $this.children('img'),
+				$imgSrc = $img.attr('src'),
+				$imgAlt = $img.attr('data-alt'),
+				$imgExt = $imgAlt.split('.');
+				
+				if($imgExt[1] === 'gif') {
+					$img.attr('src', $img.data('alt')).attr('data-alt', $imgSrc);
+				} else {
+					$img.attr('src', $imgAlt).attr('data-alt', $img.data('alt'));
+				}
+
 	});
 
 })(jQuery);
